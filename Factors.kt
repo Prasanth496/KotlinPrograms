aebf2e8c27ae8b95107b5c9dc0cce25ea9582d87
Prasanth496 <60653955+Prasanth496@users.noreply.github.com>
2020-03-30 18:50:38 +0530

    [prasanth]add. added Factors.kt Prpgram

---
+++ Factors.kt
@@ -0,0 +1,26 @@
+import java.util.*
+
+fun primefactors(n : Int) : Unit
+{
+    if(n%2==0)
+    {
+        var n = n/2
+        println("2"+" ")
+    }
+
+    for(i in 3..n)
+    {
+        if(n%i == 0)
+        {
+            println(i)
+        }
+    }
+}
+
+fun main(args : Array<String>)
+{
+    val scanner = Scanner(System.`in`)
+    val n : Int = scanner.nextInt()
+    primefactors(n)
+}
+
